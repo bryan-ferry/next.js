0ebb997302684498f8f3dd03eef71fec8e4a6e45
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2026-08-06 16:42:33 +0200

    fix pages router paths

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -3008,7 +3008,7 @@ export default async function build(
             outputPath.endsWith('/page') ? '/page' : '/route'
           )
         } else {
-          outputPath = path.posix.join('pages', outputPath)
+          outputPath = path.posix.join('pages', normalizePagePath(outputPath))
         }
         let entry = prerenderRoutes.get(outputPath)
         if (!entry) {
